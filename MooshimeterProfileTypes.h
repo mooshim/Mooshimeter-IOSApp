fda76d2f1919854653556167d39c290e59889307
whong <jwhong@users.noreply.github.com>
2014-02-11 05:36:59 +0000

    Added comments

--- MooshimeterProfileTypes.h
+++ MooshimeterProfileTypes.h
@@ -32,17 +32,17 @@ typedef enum
 : uint8
 #endif
 {
-    METER_SHUTDOWN=0,
-    METER_STANDBY,
-    METER_PAUSED,
-    METER_RUNNING,
-    METER_ONESHOT,
-    METER_CALIBRATING,
+    METER_SHUTDOWN=0,   // Booting from power down.
+    METER_STANDBY,      // uC sleeping, ADC powered down
+    METER_PAUSED,       // uC active, ADC active, not sampling
+    METER_RUNNING,      // uC active, ADC active, sampling until buffer is full, then performing computations and repeating
+    METER_ONESHOT,      // uC active, ADC active, sampling until buffer is full, performing computations and dropping back to METER_PAUSED
+    METER_CALIBRATING,  // uC active, ADC active, uC will override user ADC settings to run a cal routine and drop back to METER_PAUSED when finished
 } meter_state_t;
 
 typedef struct {
-    uint8 cal_target :7;
-    uint8 save       :1;
+    uint8 cal_target :7; // Specify which calibration slot to use in flash
+    uint8 save       :1; // Set a 1 here to save to flash
 }
 #ifndef __IAR_SYSTEMS_ICC__
 __attribute__((packed)) 
@@ -95,15 +95,15 @@ __attribute__((packed))
 MeterCalPoint_t;
 
 typedef struct {
-    meter_state_t target_meter_state;
-    meter_state_t present_meter_state;
+    meter_state_t target_meter_state;    // The target state of the meter
+    meter_state_t present_meter_state;   // The state of the meter right now.  Read only.
     cal_setting_t cal_setting;           // Specifies the buffer in to which the result of the calibration will be written
-    trigger_settings_t trigger_settings;
-    uint8 buf_depth_log2 : 4;
-    uint8 calc_mean      : 1;
-    uint8 calc_ac        : 1;
-    uint8 calc_freq      : 1;
-    uint8 fill           : 1;
+    trigger_settings_t trigger_settings; // Specifies level and rising/falling edge for pause (UNUSED)
+    uint8 buf_depth_log2 : 4;            // Determines how deep a sample buffer to capture and average.  Depth = 2^(buf_depth_log2)
+    uint8 calc_mean      : 1;            // Flag.  True = means are calculated and chx_reading_lsb in MeterMeasurement_t is populated
+    uint8 calc_ac        : 1;            // Flag.  True = mean squares are calculated and ac_chx_ms in MeterMeasurement_t is populated
+    uint8 calc_freq      : 1;            // Flag.  True = ch2_period in MeterMeasurement_t is populated
+    uint8 fill           : 1;            // Ignored.
 }
 #ifndef __IAR_SYSTEMS_ICC__
 __attribute__((packed)) 
@@ -111,12 +111,12 @@ __attribute__((packed))
 MeterSettings_t;
 
 typedef struct {
-    int24_test ch1_reading_lsb;
-    int24_test ch2_reading_lsb;
-    uint32 ac_ch1_ms;
-    uint32 ac_ch2_ms;
-    uint16 ch2_period;
-    int16 power_factor;
+    int24_test ch1_reading_lsb; // Mean of the sample buffer
+    int24_test ch2_reading_lsb; // Mean of the sample buffer
+    uint32 ac_ch1_ms;           // Mean-square of the sample buffer (TODO)
+    uint32 ac_ch2_ms;           // Mean-square of the sample buffer (TODO)
+    uint16 ch2_period;          // Period of the signal in the sample buffer (fixed point, 8 bits integral, 8 bits fractional) (TODO)
+    int16 power_factor;         // Power factor, fixed point, 16 bits fractional (TODO)
 }
 #ifndef __IAR_SYSTEMS_ICC__
 __attribute__((packed)) 
@@ -125,7 +125,7 @@ MeterMeasurement_t;
 
 #ifndef __IAR_SYSTEMS_ICC__
 typedef struct {
-    uint8 id;
+    uint8 id;                   // Information on these registers can be found on the ADS1292 datasheet.
     uint8 config1;
     uint8 config2;
     uint8 loff;
