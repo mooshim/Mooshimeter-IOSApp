1639b319ebdd5e1ac0887e652adc655bbaaef9cd
James Whong <james.whong@gmail.com>
2015-01-25 12:41:25 -0800

    Added license headers

--- Mooshimeter/SettingViewController.m
+++ Mooshimeter/SettingViewController.m
@@ -1,10 +1,20 @@
-//
-//  mooshimeterDetailViewController.m
-//  Mooshimeter
-//
-//  Created by James Whong on 9/3/13.
-//  Copyright (c) 2013 mooshim. All rights reserved.
-//
+/**************************
+Mooshimeter iOS App - interface to Mooshimeter wireless multimeter
+Copyright (C) 2015  James Whong
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+***************************/
 
 #import "SettingViewController.h"
 
