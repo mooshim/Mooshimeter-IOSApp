ac3be734357676c63fa321a8d53237ffa90e8894
James Whong <james.whong@gmail.com>
2015-02-10 22:56:39 -0800

    Added a handy script to generate all the required icon sizes

---
+++ Mooshimeter/Images.xcassets/AppIcon.appiconset/generate_sizes.py
@@ -0,0 +1,17 @@
+import os
+
+# list of target sizes, (pixels, (scales))
+sizes = [(29,(1,2,3)), (40,(2,3)), (57,(1,2)), (60,(2,3)), (29,(1,2)), (40,(1,2)), (50,(1,2)), (72,(1,2)), (76,(1,2)), (120,(1,))]
+
+fin  = "icon.png"
+fout_format = "icon-%d_%d.png"
+
+for pix, scales in sizes:
+	for s in scales:
+		fout = fout_format%(pix,s)
+		res = s*pix
+		os.system("rm %s"%fout)
+		os.system("cp -v %s %s"%(fin,fout))
+		os.system("sips -Z %d %s"%(res,fout))
+
+print "Done"
