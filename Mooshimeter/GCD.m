299608a0680e135d3442fbe609645bedc1e08a8e
James Whong <james.whong@gmail.com>
2016-10-11 14:15:25 -0700

    Corrects error in GCD syncMain

--- Mooshimeter/GCD.m
+++ Mooshimeter/GCD.m
@@ -16,7 +16,11 @@
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT,0),block);
 }
 + (void)syncMain:(void (^)())block {
-    dispatch_async(dispatch_get_main_queue(),block);
+    if([NSThread isMainThread]) {
+        block();
+    } else {
+        dispatch_sync(dispatch_get_main_queue(),block);
+    }
 }
 + (void)asyncMainAfterMS:(int)ms block:(void (^)())block {
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW,NSEC_PER_MSEC*ms),dispatch_get_main_queue(),block);
